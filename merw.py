2d6d3476d5ddc624858402668bdce12b34ab3989
Tomasz Roszczynialski <roszcz@gmail.com>
2015-09-01 03:53:30 +0200

    Merw within potentials

--- merw.py
+++ merw.py
@@ -6,8 +6,7 @@ import random
 import bisect
 import collections
 
-N_ = 12
-k_ = 2
+N_ = 80
 
 def cdf(weights):
     total = sum(weights)
@@ -32,6 +31,7 @@ def calc_S(A):
     for it in range(N_):
         for jt in range(N_):
             if V[jt] == 0:
+                print 'this should never happen'
                 S[it,jt] = 0.
             else:
                 S[it,jt] = V[it]/V[jt] * A[it,jt]/d
@@ -52,15 +52,30 @@ def they_interact(i, j):
     # Else
     return False
 
+def merw_pot(i):
+    prefered = 60
+    ret = 1.5 - ((i-prefered)/float(N_))**2
+    return ret
+# Show potential 
+x = range(N_)
+y = [merw_pot(s) for s in x]
+plt.plot(x,y)
+plt.show()
+
 
 # Calculate probability matix holding S
-A = np.random.random((N_, N_))/1e4
+A = np.random.random((N_, N_))/1e20
+A = np.zeros(A.shape)
+noise = 1e-4
 for it in range(N_):   
-    for jt in range(it-8, it+9): # Wtf python
+    for jt in range(it-3, it+4): # Wtf python
         if jt >= 0 and jt < N_:
             if they_interact(it,jt):
-    #            print it, jt
-                A[it, jt] = 10
+                A[it, jt] = 5 
+                A[it, jt] *= merw_pot(it)
+            else:
+                A[it, jt] += noise
+
 #        if abs(it-jt) == 1 and jt>=0 and jt < N_:
 #            A[it, jt] = 0.123
 
@@ -75,9 +90,11 @@ values = range(N_)
 
 # Merw starting position set
 pos = 1
+positions = []
 
 # Walk
-for it in range(int(5e4)):
+for it in range(int(4e2)):
+    positions.append(pos)
     score[pos] += 1
     pos = randomly_draw(values, S[:,pos])
 
@@ -86,4 +103,7 @@ plt.imshow(S)
 plt.show()
 
 plt.plot(score,'ko')
+plt.show()
+
+plt.plot(positions, 'ko')
 plt.show()
