47f3f8d92f4d3be99db05bda41f32601d1e5ca9a
Tomasz Roszczynialski <roszcz@gmail.com>
2015-09-02 03:04:34 +0200

    Major reordering, prefered position time dependent

--- merw.py
+++ merw.py
@@ -1,109 +1,71 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.linalg as lg
-# For probability
-import random
-import bisect
-import collections
+from potentials import *
 
-N_ = 80
+class Merw(object):
+    def __init__(self, size):
+        self.size = size
+        self.values = range(self.size)
+        self.pot = E_potential(self.size)
+        self.S = self.pot.get_S()
+        self.A = self.pot.get_A()
+    def set_prefered(self, prefered):
+        self.pot.set_prefered(prefered)
+    def get_S(self):
+        return self.pot.get_S()
+    def get_A(self):
+        return self.pot.get_A()
+    def get_next_value(self, idx, time=0):
+        # idx - previous index
+        # time - variable to control potential
+        S = self.pot.get_S()
+        return randomly_draw(self.values, S[:,idx])
 
-def cdf(weights):
-    total = sum(weights)
-    result = []
-    cumsum = 0
-    for w in weights:
-        cumsum += w
-        result.append(cumsum / total)
-    return result
+if __name__ == '__main__':
 
-def choice(weights):
-    # Normalize weights to 1! XXX
-    cdf_vals = cdf(weights)
-    x = random.random()
-    idx = bisect.bisect(cdf_vals, x)
-    return idx
+    # Calculate probability matix holding S
+    merw = Merw(N_)
 
+    # Prepare histogram
+    score = np.zeros(N_)
 
-def calc_S(A):
-    S = np.zeros(A.shape)
-    d, V = lg.eigh(A, eigvals = (N_-1, N_-1))
+    # Prepare list of indexes
+    values = range(N_)
+
+    # Merw starting position set
+    pos = 33
+    positions = []
+
+    # Walk
+    intervals = []
+
+    # Prepare meta potential for testing
+    # Number of iterations
+    N_ = 250
+    def meta_pot(it):
+        ret = 70.0 * (0.5 * (1.0 + np.cos(2.*it*np.pi/N_)))
+        return ret
+
     for it in range(N_):
-        for jt in range(N_):
-            if V[jt] == 0:
-                print 'this should never happen'
-                S[it,jt] = 0.
-            else:
-                S[it,jt] = V[it]/V[jt] * A[it,jt]/d
-    return S
+        positions.append(pos)
+        next_pos = merw.get_next_value(pos)
+        merw.set_prefered(int(meta_pot(it)))
 
-def randomly_draw(values, q):
-    # q is of course probabilities
-    idx = choice(q)
-    return values[idx]
+        # Histogram
+        score[pos] += 1
 
-def they_interact(i, j):
-    scale = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]
-    n = len(scale)
+        # Intervals plot
+        intervals.append(abs(next_pos - pos))
 
-    if scale[i%n] == scale[j%n] == 1: # Nice python!
-        return True
+        # Switcheroo
+        pos = next_pos
 
-    # Else
-    return False
-
-def merw_pot(i):
-    prefered = 60
-    ret = 1.5 - ((i-prefered)/float(N_))**2
-    return ret
-# Show potential 
-x = range(N_)
-y = [merw_pot(s) for s in x]
-plt.plot(x,y)
-plt.show()
+        if (it%50 == 1):
+            print it, '/', N_
 
 
-# Calculate probability matix holding S
-A = np.random.random((N_, N_))/1e20
-A = np.zeros(A.shape)
-noise = 1e-4
-for it in range(N_):   
-    for jt in range(it-3, it+4): # Wtf python
-        if jt >= 0 and jt < N_:
-            if they_interact(it,jt):
-                A[it, jt] = 5 
-                A[it, jt] *= merw_pot(it)
-            else:
-                A[it, jt] += noise
+    S = merw.get_S(); plt.imshow(S); plt.show()
 
-#        if abs(it-jt) == 1 and jt>=0 and jt < N_:
-#            A[it, jt] = 0.123
+    #plt.plot(score,'ko'); plt.show()
 
-# Here
-S = calc_S(A)
-
-# Prepare histogram
-score = np.zeros(N_)
-
-# Prepare list of indexes
-values = range(N_)
-
-# Merw starting position set
-pos = 1
-positions = []
-
-# Walk
-for it in range(int(4e2)):
-    positions.append(pos)
-    score[pos] += 1
-    pos = randomly_draw(values, S[:,pos])
-
-
-plt.imshow(S)
-plt.show()
-
-plt.plot(score,'ko')
-plt.show()
-
-plt.plot(positions, 'ko')
-plt.show()
+    plt.plot(positions, 'ko'); plt.show()
---
+++ potentials.py
@@ -0,0 +1,115 @@
+import numpy as np
+# For probability
+import random
+import bisect
+import collections
+import scipy.linalg as lg
+
+N_ = 80
+
+
+
+
+# TODO - circular conditions
+# TODO - move A-creation to separate method
+
+
+def cdf(weights):
+    total = sum(weights)
+    result = []
+    cumsum = 0
+    for w in weights:
+        cumsum += w
+        result.append(cumsum / total)
+    return result
+
+def choice(weights):
+    # Normalize weights to 1! XXX
+    cdf_vals = cdf(weights)
+    x = random.random()
+    idx = bisect.bisect(cdf_vals, x)
+    return idx
+
+def randomly_draw(values, q):
+    # q is of course probabilities
+    idx = choice(q)
+    return values[idx]
+
+
+class E_potential(object):
+    def __init__(self, size):
+        self.size = size
+        self.prefered = 0
+        self.calc_A()
+#        self.simple_A()
+        self.calc_S()
+
+    def set_prefered(self, prefered):
+        self.prefered = prefered
+        self.calc_A()
+        self.calc_S()
+
+    def they_interact(self, i, j):
+        scale = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]
+        n = len(scale)
+
+        if scale[i%n] == scale[j%n] == 1: # Nice python!
+            return True
+
+        # Else
+        return False
+
+    def merw_pot(self, i, prefered = 0):
+        ret = 1. - ((i-prefered)/float(self.size))**2
+        return ret
+
+    def get_potential(self):
+        # Show potential 
+        x = range(self.size)
+        y = [self.merw_pot(s) for s in x]
+        plt.plot(x,y)
+        plt.show()
+
+    def get_A(self):
+        return self.A
+    def get_S(self):
+        return self.S
+
+    def simple_A(self):
+        A = np.zeros((self.size, self.size))
+        k = 3
+        for it in range(self.size):
+            for jt in range(it-k ,it+k+1):
+                if jt == it + 1 or jt == it -1:
+                    if jt >= 0 and jt < self.size:
+                        A[it,jt] = 1
+        self.A = A
+
+    def calc_A(self):
+        A = np.random.random((self.size, self.size))/1e3
+        A = np.zeros(A.shape)
+        noise = 1e-4
+        k = 4
+        for it in range(self.size):
+            for jt in range(it-k, it+k+1): # Wtf python
+                if jt >= 0 and jt < self.size:
+                    if self.they_interact(it,jt):
+                        A[it, jt] = 5
+                        A[it, jt] *= self.merw_pot(it, self.prefered)
+                    else:
+                        A[it, jt] += noise
+        self.A = A
+
+    def calc_S(self):
+        S = np.zeros(self.A.shape)
+        d, V = lg.eigh(self.A, eigvals = (N_-1, N_-1))
+        for it in range(N_):
+            for jt in range(N_):
+                if V[jt] == 0:
+                    print 'this should never happen'
+                    S[it,jt] = 0.
+                else:
+                    S[it,jt] = V[it]/V[jt] * self.A[it,jt]/d
+        self.S = S
+
+
