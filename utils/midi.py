10893d1e8bda8eb5224bf4a6ba7f7bbca1f16171
Tomasz Roszczynialski <roszcz@gmail.com>
2016-04-03 01:38:11 +0200

    Add simple piano roll generator

--- utils/midi.py
+++ utils/midi.py
@@ -1,9 +1,10 @@
+from matplotlib import pyplot as plt
 import numpy as np
 from midiutil.MidiFile import MIDIFile
 
 def matrix_to_midi(notes, filename = 'dupa.mid'):
     """ Simplify midi generation
-        notes format: PITCH|TIME|DURATION|VOLUME """
+        notes format: PITCH|START|DURATION|VOLUME """
     # Midi file with one track
     mf = MIDIFile(1)
 
@@ -22,6 +23,33 @@ def matrix_to_midi(notes, filename = 'dupa.mid'):
     with open(filename, 'wb') as fout:
         mf.writeFile(fout)
 
+def show_piano_roll(notes):
+    """ Another properly named function """
+    # Number of avaiable pitches 
+    nof_rows = 101
+
+    # Get max time (time of start + duration)
+    total_time = notes[-1][1] + notes[-1][2]
+
+    # Get number of samples
+    dt = 2**-4
+    nof_samples = total_time / dt + 1
+    roll = np.zeros((nof_rows, nof_samples))
+
+    for note in notes:
+        # Pitch - 3 ::: index 
+        note_it = note[0] - 30
+
+        # Time ids
+        start_id = note[1] / dt
+        end_id = start_id + note[2] / dt
+
+        # Volume dependant color
+        roll[note_it, start_id:end_id] = 127.0 / note[3]
+
+    plt.imshow(roll, aspect=4)
+    plt.show()
+
 def test():
     """ Example usage """
     pitches = [60 + 2 * it for it in range(20)]
@@ -35,4 +63,4 @@ def test():
     # Change to file
     matrix_to_midi(notes, 'yo.mid')
 
-    print 'yo'
+    return notes
