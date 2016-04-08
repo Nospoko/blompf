97f603ce9155b4b35b876d1edc5b4f13a91960f8
Tomasz Roszczynialski <roszcz@gmail.com>
2016-04-08 22:21:42 +0200

    Bring back walkers with prefered position

--- main.py
+++ main.py
@@ -3,17 +3,37 @@ import matplotlib.pyplot as plt
 from utils import midi as um
 from walkers import finger as wf
 from walkers import hand as wh
+from walkers import merw as wm
 
 if __name__ == '__main__':
     """ what up """
     # How many steps will walker walk
-    nof_steps = 250
-    hand = wh.ExampleHand()
+    nof_steps = 500
+
+    biased = wm.BiasedWalker(20)
+    path = [biased.current_value()]
+
+    wavy = lambda x: np.cos(2.0 * x) * np.sin(5.0 * x)
+    prefered = lambda x: 50 + 40 * wavy(7.2 * x / nof_steps)
 
     for tick in range(nof_steps):
-        hand.play(tick)
+        biased.set_bias(prefered(tick))
+        path.append(biased.next_value())
 
-    um.show_piano_roll(hand.get_notes())
+        if tick%10 is 0:
+            print tick
+
+    biased.show_histogram()
+
+    plt.plot(path, 'ko')
+    plt.show()
+
+    # hand = wh.ExampleHand()
+    #
+    # for tick in range(nof_steps):
+    #     hand.play(tick)
+    #
+    # um.show_piano_roll(hand.get_notes())
 
     # TODO New interface:
     # hands = create_hands()
--- walkers/merw.py
+++ walkers/merw.py
@@ -1,7 +1,7 @@
 import numpy as np
-from matplotlib import pyplot as plt
 import scipy.linalg as lg
 from utils import probability as up
+from matplotlib import pyplot as plt
 
 class Merwer(object):
     """ Most abstract maximal enthropy random walker """
@@ -32,8 +32,8 @@ class Merwer(object):
     def make_A(self):
         """ Prepare A-matrix """
         A = np.zeros((self.size, self.size))
-        # Length of a 
-        max_dist = 1
+        # Length of a possible jump
+        max_dist = 5
         for it in range(self.size):
             for jt in range(it - max_dist, it + max_dist + 1):
                 if jt >= 0 and jt < self.size:
@@ -80,18 +80,18 @@ class Merwer(object):
         # Return value
         return self.values[self.current_id]
 
-    def update_S(self):
-        """ Re-do the S matrix """
-        if self.S_update_needed:
-            self.make_A()
-            self.make_S()
-
-    # FIXME this is not most abstract
+    # FIXME those are not most abstract
     def set_interaction_grid(self, grid):
         """ This defines possible transitions """
         # Something like:
         # grid = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1] * 8
         self.interaction_grid = grid
+
+    def update_S(self):
+        """ Re-do the S matrix """
+        if self.S_update_needed:
+            self.make_A()
+            self.make_S()
 
     def set_emotional_parameter(self, happy):
         """ This shall grow """
@@ -99,7 +99,43 @@ class Merwer(object):
 
 class VolumeWalker(Merwer):
     """ Simplest neeeded implementation of mer walker """
-    def __init__(self, values, first_id):
+    def __init__(self, first_id):
         """ fin: rakentaja """
+        # Possible midi volumes
+        values = range(128)
         # Construct parent
         Merwer.__init__(self, values, first_id)
+
+# TODO Add some atracting mechanism causing merw to 
+# walk in some desired direction
+class BiasedWalker(Merwer):
+    """ Walker atracted to some value """
+    def __init__(self, first_id):
+        """ Konstrutkor """
+        values = range(128)
+        self.bias = int(len(values)/2)
+        Merwer.__init__(self, values, first_id)
+
+    def set_bias(self, prefered):
+        """ Sets prefered value """
+        self.bias = prefered
+
+        # Update probabilities
+        self.make_S()
+
+    def A_it_jt(self, it):
+        """ Aij definition (symmetric) """
+        out = 1 - 1.0 * abs(self.bias - it)/self.size
+
+        return out
+
+    def make_A(self):
+        """ Special atraction matrix """
+        A = np.zeros((self.size, self.size))
+
+        max_dist = 5
+        for it in range(self.size):
+            for jt in range(it - max_dist, it + max_dist + 1):
+                if jt >= 0 and jt < self.size:
+                    A[it, jt] = self.A_it_jt(it)
+        self.A = A
